bd39fe068847d5be87a4babacd8a3c25f96ad0af
j-hc <karayay12@gmail.com>
2022-03-08 19:10:04 +0300

    sort by rank

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Türkçe Kelimeler Frekans Sözlüğü
+# Türkçe Kelimeler için Frekans/Sıklık Sözlüğü
 # A Frequency Dictionary of Turkish
 
 Veri [A Frequency Dictionary of Turkish](https://books.google.com.tr/books/about/A_Frequency_Dictionary_of_Turkish.html?id=HjslDwAAQBAJ) kitabından alınıp CSV formatında parselanmıştır.
--- parser.py
+++ parser.py
@@ -5,27 +5,35 @@ import os
 
 RE_WORD = re.compile(
     r"^([0-9]+) ([a-zçğıöşü]+) ((?:(?:adv|adj|v|n|posp|det|conj|posp)(?:, )?){1,2}).+?\n([0-9]+) (0\.[0-9]+)",  flags=re.DOTALL | re.MULTILINE)
-TXTS_DIR = r"./txts" # OCR'd book pages
+TXTS_DIR = r"./txts"  # OCR'd book pages
 CSV_DIR = r"./turkish-words-freq.csv"
 
+
+dic = []
+for f in os.listdir(TXTS_DIR):
+    fpath = os.path.join(TXTS_DIR, f)
+    with open(fpath, "r") as txt:
+        def rep(w: tuple):
+            w = list(w)
+            t = w[2]
+            if ',' in t:
+                w[2] = t.replace(', ', '/')
+                return tuple(w)
+            else:
+                return tuple(w)
+        parsed_row = list(map(rep, RE_WORD.findall(txt.read())))
+        assert all(len(r) == 5 for r in parsed_row), parsed_row
+        dic.extend(parsed_row)
+
+
+dic = sorted(dic, key=lambda w: int(w[0]))
 
 with open(CSV_DIR, "w+", newline='') as parsed_f:
     csv_writer = csv.writer(parsed_f)
     csv_writer.writerow(
         ["rank", "headword", "part of speech", "frequency", "dispersion"])
-    for f in sorted(os.listdir(TXTS_DIR)):
-        fpath = os.path.join(TXTS_DIR, f)
-        with open(fpath, "r") as txt:
-            def rep(w: tuple):
-                w = list(w)
-                t = w[2]
-                if ',' in t:
-                    w[2] = t.replace(', ', '/')
-                    return tuple(w)
-                else:
-                    return tuple(w)
-            parsed_row = list(map(rep, RE_WORD.findall(txt.read())))
-            assert all(len(r) == 5 for r in parsed_row), parsed_row
-            csv_writer.writerows(parsed_row)
+    csv_writer.writerows(dic)
+
+
 
 print("completed")
